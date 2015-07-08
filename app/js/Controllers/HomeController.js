728d646c61ff3daeccb196e333cfbda68b1cde98
didimitrov <di_dimitrov_@abv.bg>
2015-07-08 15:50:50 +0300

    Implement HomeController.js and Home.html (show all ads)

--- app/js/Controllers/HomeController.js
+++ app/js/Controllers/HomeController.js
@@ -1,5 +1,20 @@
 'use strict'
 
-app.controller('HomeController',function($scope,$rootScope,authService,notifyService,adsService){
+app.controller('HomeController',function($scope,notifyService,adsService,pageSize){
+    $scope.adsParams={
+        'startPage': 1,
+        'pageSize': pageSize
+    };
 
+    $scope.reloadAds =function(){
+        adsService.getAds($scope.adsParams,
+            function success(data){
+            $scope.ads=data;
+        },
+            function error(err){
+            notifyService.showError("Cannot load ads", err)
+        })
+    }
+
+    $scope.reloadAds();
 });
