f25e625aa348f4d09e6e8351df0fce27ff99dc5e
didimitrov <di_dimitrov_@abv.bg>
2015-07-09 11:09:53 +0300

    Display categories and towns

--- app/js/Controllers/HomeController.js
+++ app/js/Controllers/HomeController.js
@@ -17,4 +17,18 @@ app.controller('HomeController',function($scope,notifyService,adsService,pageSiz
     };
 
     $scope.reloadAds();
+
+    // This event is sent by RightSideBarController when the current category is changed
+    $scope.$on("categorySelectionChanged", function(event, selectedCategoryId) {
+        $scope.adsParams.categoryId = selectedCategoryId;
+        $scope.adsParams.startPage = 1;
+        $scope.reloadAds();
+    });
+
+    // This event is sent by RightSideBarController when the current town is changed
+    $scope.$on("townSelectionChanged", function(event, selectedTownId) {
+        $scope.adsParams.townId = selectedTownId;
+        $scope.adsParams.startPage = 1;
+        $scope.reloadAds();
+    });
 });
--- app/js/Controllers/RightSidebarController.js
+++ app/js/Controllers/RightSidebarController.js
@@ -1,5 +1,16 @@
 'use strict'
 
-app.controller('RightSidebarController', function($scope,$location,authService,notifyService){
+app.controller('RightSidebarController',
+    function($scope,$location,categoryService,townsService,notifyService){
+        $scope.towns=townsService.getTowns();
+        $scope.categories = categoryService.getCategories();
 
-});
+        $scope.categoryClicked = function(clickedCategoryId) {
+            $scope.selectedCategoryId = clickedCategoryId;
+        };
+
+        $scope.townClicked =function(clickedTownId){
+            $scope.selectedTownId=clickedTownId;
+        }
+
+    });
